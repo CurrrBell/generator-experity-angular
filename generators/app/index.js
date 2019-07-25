be4ef8f1fca0766580c4b05cb92bbb659fb77062
Chris Bell <chbell@practicevelocity.com>
2019-07-25 14:36:45 -0500

    make new directory, format to kebab-case, init git

--- generators/app/index.js
+++ generators/app/index.js
@@ -26,8 +26,7 @@ module.exports = class extends Generator {
           {
             type: "input",
             name: "appName",
-            message: "App name (default in parentheses):",
-            default: this.appname
+            message: "Enter a name for your new app:"
           }
         ]);
 
@@ -81,7 +80,12 @@ module.exports = class extends Generator {
     }
 
     this.formatAppName = function (appName) {
+      // remove spaces
       appName = appName.split(' ').join('-');
+
+      // convert from camelCase/PascalCase to kebab-case.
+      // * lifted from https://gist.github.com/nblackburn/875e6ff75bc8ce171c758bf75f304707
+      appName = appName.replace(/([a-z0-9])([A-Z])/g, '$1-$2');
 
       // as of 7/2019, this is our naming standard
       if (!appName.endsWith('--ui')) {
@@ -109,32 +113,31 @@ module.exports = class extends Generator {
   }
 
   writing() {
-
     this.fs.copyTpl(
       this.sourceRoot(),
-      this.destinationRoot(),
+      this.destinationPath(`${this.answers.appName}`),
       { appName: this.answers.appName }
     );
 
     // Copy all dotfiles
     this.fs.copy(
       this.templatePath('./.*'),
-      this.destinationRoot(),
+      this.destinationPath(`${this.answers.appName}`),
       { appName: this.answers.appName }
     );
 
     // For some reason, the .vscode files don't copy unless you do it explicitly. Probably user error but code below works
     this.fs.copy(
       this.templatePath('./.vscode/'),
-      this.destinationPath('./.vscode/')
+      this.destinationPath(`${this.answers.appName}/.vscode/`)
     );
   }
 
   install() {
-    this.spawnCommandSync('npm', ['install']);
-    this.spawnCommandSync('git', ['init']);
-    this.spawnCommandSync('git', ['add', '.']);
-    this.spawnCommandSync('git', ['commit', '-m', 'initial commit from Yeoman generator']);
-    this.spawnCommandSync('code', ['.', '-g', 'README.md']);
+    this.spawnCommandSync('npm', ['install'], { cwd: `${this.answers.appName}` });
+    this.spawnCommandSync('git', ['init'], { cwd: `${this.answers.appName}` });
+    this.spawnCommandSync('git', ['add', '.'], { cwd: `${this.answers.appName}` });
+    this.spawnCommandSync('git', ['commit', '-m', 'initial commit from Yeoman generator'], { cwd: `${this.answers.appName}` });
+    this.spawnCommandSync('code', ['.', '-g', 'README.md'], { cwd: `${this.answers.appName}` });
   }
 };
